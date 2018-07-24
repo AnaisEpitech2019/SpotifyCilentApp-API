4526e2375701e474d4f0f3685f69330454032e6a
anaisfoncel <anais.foncel@epitech.eu>
2018-07-24 20:26:10 +0200

    UPDATE Recovery AllCategories (offset and limit)

--- app/src/main/java/com/spotifyclientapp/anais/spotifyclientapp_api/managers/HomeManager.java
+++ app/src/main/java/com/spotifyclientapp/anais/spotifyclientapp_api/managers/HomeManager.java
@@ -4,16 +4,24 @@ import com.spotifyclientapp.anais.spotifyclientapp_api.callbacks.MyCallback;
 import com.spotifyclientapp.anais.spotifyclientapp_api.models.modelsAll.AllCategories;
 import com.spotifyclientapp.anais.spotifyclientapp_api.services.HomeServices;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import retrofit2.Call;
 
 public class HomeManager {
 
-    public static Call<AllCategories> getAllCategories(String extension, MyCallback<AllCategories> callback) {
+    public static Call<AllCategories> getAllCategories(String offset, String limit, MyCallback<AllCategories> callback) {
+
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("offset", offset);
+        map.put("limit", limit);
 
         Call<AllCategories> call = APIManager.buildService(HomeServices.getAllCategories.class)
                 .load(
                         "Bearer " + APIManager.getAuthToken(),
-                        extension
+                        map
                 );
 
         call.enqueue(APIManager.createAPICallback(callback));
--- app/src/main/java/com/spotifyclientapp/anais/spotifyclientapp_api/services/HomeServices.java
+++ app/src/main/java/com/spotifyclientapp/anais/spotifyclientapp_api/services/HomeServices.java
@@ -2,18 +2,21 @@ package com.spotifyclientapp.anais.spotifyclientapp_api.services;
 
 import com.spotifyclientapp.anais.spotifyclientapp_api.models.modelsAll.AllCategories;
 
+import java.util.Map;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Path;
+import retrofit2.http.QueryMap;
 
 public class HomeServices {
 
     public interface getAllCategories {
-        @GET("/v1/browse/categories{extension}")
+        @GET("/v1/browse/categories")
         Call<AllCategories> load(
                 @Header("Authorization") String authHeader,
-                @Path("extension") String extension
-        );
+                @QueryMap Map<String, Object> map
+                );
     }
 }
